8e4344f2ac5db6846a58add6cfc6219480ae91d2
dgenutis <dainiusgenutis@yahoo.com>
2024-11-13 10:54:30 +0200

    mini korection in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {InputBox} from './components/index.js';
 function App() {
   const [amount, setAmount] = useState(0)
   const [from, setFrom] = useState('usd')
-  const [to, setTo] = useState('inr')
+  const [to, setTo] = useState('usd')
   const [convertedAmount, setConvertedAmount] = useState(0)
 
 
